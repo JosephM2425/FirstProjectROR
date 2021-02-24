adf6e85167ab3bef57478eb1d4dd48097c842b98
Joseph Murillo <josephmurillo2425@gmail.com>
2021-02-24 11:08:57 -0600

    Controlador three y pagina

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   get 'one', to: 'pages#one'
   get 'two', to: 'pages#two'
+  get 'three', to: 'pages#three'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
